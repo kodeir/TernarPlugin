df9e940b63fc9724fc951232f13216c5cba6597f
Kodeir <kodeir@gmail.com>
2017-03-12 23:38:34 +0300

    simple text version

---
+++ src/com/kodeir/plugin/ternary/TernaryCreator.java
@@ -0,0 +1,52 @@
+package com.kodeir.plugin.ternary;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+
+/**
+ * Created by Sergei "Kodeir" Riabinin on 12/03/17.
+ *
+ */
+public class TernaryCreator extends AnAction {
+
+    private Project project;
+    private Document document;
+    private CaretModel caretModel;
+
+    @Override
+    public void actionPerformed(AnActionEvent actionEvent) {
+        getProjectData(actionEvent);
+        WriteCommandAction.runWriteCommandAction(project, run());
+    }
+
+    /**
+     * get project & editor
+     * @param actionEvent AnActionEvent
+     */
+    private void getProjectData(AnActionEvent actionEvent){
+        project = actionEvent.getData(PlatformDataKeys.PROJECT);
+        Editor editor = actionEvent.getRequiredData(CommonDataKeys.EDITOR);
+
+        document = editor.getDocument();
+        caretModel = editor.getCaretModel();
+    }
+
+    private Runnable run(){
+        return () -> {
+            int txtLineEnd = caretModel.getOffset();
+            String newText = createTernaryText();
+            document.insertString(txtLineEnd, newText);
+        };
+    }
+
+    private String createTernaryText() {
+        return ("checkCondition() ? doIfTrue() : doIfFalse();");
+    }
+}
